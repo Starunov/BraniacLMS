ffd69be510a7459e09d59396f9ab88399cfb813a
Sergey Starunov <35844513@bk.ru>
2022-12-03 18:34:55 +0300

    lesson_6 Feedbacks with AJAX

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -14,6 +14,7 @@ urlpatterns = [
     path("news/<int:pk>/delete", views.NewsDeleteView.as_view(), name="news_delete"),
     path("courses/", views.CoursesListView.as_view(), name="courses"),
     path("courses/<int:pk>/", views.CoursesDetailView.as_view(), name="courses_detail"),
+    path("course_feedback/", views.CourseFeedbackFormProcessView.as_view(), name="course_feedback"),
     path("contacts/", views.ContactsPageView.as_view(), name="contacts"),
     path("doc_site/", views.DocSitePageView.as_view(), name="doc_site"),
 ]
